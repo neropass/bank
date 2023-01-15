0d3ec5994eb872a4bfce27860d21689a7b7db1f2
neropass <drakonobor@gmail.com>
2023-01-15 16:48:04 +0300

    Finished Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
         <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => setToggle((prev) => !prev)} />
 
         <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sudebar`}>
-          <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+          <ul className='list-none flex justify-end flex-col items-center flex-1'>
             {navLinks.map((nav, index) => (
-              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
+              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
                 <a href={`#${nav.id}`}>
                   {nav.title}
                 </a>
